139501c6352c7f7fe297dcea4b51aba0300a446c
AnastasiyaKharitonenko <concordia85@gmail.com>
2023-02-12 18:26:03 +0400

    добавила модальное окно(закр/откр)

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const addBox = document.querySelector(".add-box");
-modalWindow = document.querySelector(".modal-window");
-closeIcone = modalWindow.querySelector("header i");
-
-
-addBox.addEventListener("click", () => {
-	modalWindow.classList.add("show");
-})
-closeIcone.addEventListener("click", () => {
-	modalWindow.classList.remove("show");
-})
+let modal = document.querySelector(".modal-window");
+let btn = document.querySelector(".add-box");
+// Get the <span> element that closes the modal
+let closeWindow = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+closeWindow.onclick = function() {
+	modal.style.display = "none";
+ }
+ 
+btn.onclick = function(){
+	modal.style.display = "block";
+}
